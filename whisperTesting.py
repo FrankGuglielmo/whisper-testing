4c9da4bd1060710439f435b124717131ece9d581
Frank Guglielmo <frankguglielmo@Franks-MacBook-Pro-2.local>
2023-10-10 09:25:45 -0700

    Initial Testing of Whisper combined with ffmpeg

---
+++ whisperTesting.py
@@ -0,0 +1,63 @@
+import whisper
+import pprint
+import subprocess as sp
+import os
+
+
+def splitAudio(audioFile):
+    # split audio into 30 second chunks
+    sp.run(f"ffmpeg -i {audioFile} -f segment -segment_time 30 -c copy ./audioFiles/{audioFile}%03d.m4a".split())
+    # sp.run("ffmpeg -i englishAndItalianAudio.m4a -f segment -segment_time 30 -c copy ./audioFiles/audio%09d.m4a".split())
+
+def transcribeAll():
+    audio_directory = "./audioFiles"
+
+    for filename in os.listdir(audio_directory):
+        if filename.endswith(".m4a"):
+            transcribe(f"{audio_directory}/{filename}")
+            continue
+        else:
+            continue
+
+    #Delete all files in audioFiles directory
+    for filename in os.listdir(audio_directory):
+        if filename.endswith(".m4a"):
+            os.remove(f"{audio_directory}/{filename}")
+            continue
+        else:
+            continue
+
+
+
+
+
+def transcribe(audioFile):
+    model = whisper.load_model("base")
+
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audioFile)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16 = False)
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    print(result.text)
+
+# splitAudio()
+
+# transcribe("audioFiles/audio000000000.m4a")
+# transcribe("audioFiles/audio000000001.m4a")
+
+splitAudio("englishAndItalianAudio.m4a")
+transcribeAll()
+
+
